2e7746628621a5465b83574605cc7984ef4729fe
NikraOP <chpok123kolispok@gmail.com>
2026-03-26 23:31:09 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { BrowserRouter, HashRouter, Route, Routes } from 'react-router'
-import './styles/index'
+import './styles/normalize.module.scss'
+import './styles/variable.module.scss'
+import './styles/global.module.scss'
 import LeftMenu from './components/leftMenu/leftMenu'
 import PageTasks from './components/PageTasks/PageTasks'
 import RankPage from './components/rankPage/RankPage'
