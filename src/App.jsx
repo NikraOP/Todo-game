320ca35faa7bc3605252bdb7a277f24e2250cb56
NikraOP <chpok123kolispok@gmail.com>
2026-03-26 23:25:07 +0300

    initial commit for Todo-game

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, HashRouter, Route, Routes } from 'react-router'
-import LeftMenu from './components/leftMenu/leftMenu'
 import './styles/index'
+import LeftMenu from './components/leftMenu/leftMenu'
 import PageTasks from './components/PageTasks/PageTasks'
 import RankPage from './components/rankPage/RankPage'
 
